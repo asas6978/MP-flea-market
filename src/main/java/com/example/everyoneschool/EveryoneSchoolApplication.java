fff4ef6b3e0b16f2d85c136bedaf5a3809949c1a
asas6978 <asas6978@naver.com>
2023-03-24 00:50:40 +0900

    feat: 초기 환경설정

---
+++ src/main/java/com/example/everyoneschool/EveryoneSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.everyoneschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryoneSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveryoneSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/everyoneschool/EveryoneSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.everyoneschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryoneSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
