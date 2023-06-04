d92363314e9b5bfca1631ac1c4a41abbbcf8ccf5
asas6978 <asas6978@naver.com>
2023-06-04 23:17:24 +0900

    chore: 프로젝트명 수정

--- src/main/java/com/example/fleamarket/EveryoneSchoolApplication.java
+++ src/main/java/com/example/fleamarket/EveryoneSchoolApplication.java
@@ -1,4 +1,4 @@
-package com.example.fleemarket;
+package com.example.fleamarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
