22d9d9064509948f0bdd1955de97d28107440ac9
asas6978 <asas6978@naver.com>
2023-06-04 22:57:53 +0900

    feat: 상수 문자 정의

---
+++ src/main/java/com/example/fleamarket/constant/ProductConst.java
@@ -0,0 +1,8 @@
+package com.example.fleamarket.constant;
+
+public abstract class ProductConst {
+    public static final String SUCCESS_SAVE = "저장에 성공하였습니다.";
+    public static final String FAILED_SAVE = "저장에 실패하였습니다.";
+    public static final String NOT_EXIST = "존재하지 않는 상품입니다.";
+    public static final String EXIST = "존재하는 상품입니다.";
+}
